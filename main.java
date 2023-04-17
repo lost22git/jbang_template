7984dc31eab720b187ac006f232884212882ff52
lost22git <lost22git>
2023-04-17 14:14:43 +0800

    init commit

---
+++ main.java
@@ -0,0 +1,14 @@
+///usr/bin/env jbang "$0" "$@" ; exit $?
+
+import static java.lang.System.*;
+
+//NATIVE_OPTIONS --no-fallback -H:+ReportExceptionStackTraces --enable-preview --enable-monitoring=jfr
+//COMPILE_OPTIONS --enable-preview --release 19
+//RUNTIME_OPTIONS --enable-preview -XX:+UseZGC -XX:NativeMemoryTracking=summary -XX:+HeapDumpOnOutOfMemoryError -XX:StartFlightRecording=filename=jfr/,path-to-gc-roots=true,jdk.ObjectCount#enabled=true
+
+public class main {
+
+    public static void main(String... args) {
+        out.println("Hello World");
+    }
+}
