fbed15dee6692370941f1fd1363f54d4d7e100ac
lost22git <lost22git>
2023-04-19 09:00:34 +0800

    remove native option `-H:+StaticExecutableWithDynamicLibC` since error in macos

--- main.java
+++ main.java
@@ -3,7 +3,7 @@
 import static java.lang.System.*;
 
 //JAVA 19
-//NATIVE_OPTIONS --no-fallback -H:+ReportExceptionStackTraces --enable-preview --enable-monitoring=jfr -H:+StaticExecutableWithDynamicLibC
+//NATIVE_OPTIONS --no-fallback -H:+ReportExceptionStackTraces --enable-preview --enable-monitoring=jfr
 //COMPILE_OPTIONS --enable-preview --release 19
 //RUNTIME_OPTIONS --enable-preview -XX:+UseZGC -XX:NativeMemoryTracking=summary -XX:+HeapDumpOnOutOfMemoryError -XX:StartFlightRecording=filename=jfr/,path-to-gc-roots=true,jdk.ObjectCount#enabled=true
 
