ef94c1360ce8174b54a452b2c53f1bf6a5ee96d4
lost22git <lost22git>
2023-04-23 03:52:05 +0800

    NATIVE_OPTIONS --enable-monitor=jfr to --enable-monitor=all

--- main.java
+++ main.java
@@ -3,7 +3,7 @@
 import static java.lang.System.*;
 
 //JAVA 19
-//NATIVE_OPTIONS --no-fallback -H:+ReportExceptionStackTraces --enable-preview --enable-monitoring=jfr
+//NATIVE_OPTIONS --no-fallback -H:+ReportExceptionStackTraces --enable-preview --enable-monitoring
 //COMPILE_OPTIONS --enable-preview --release 19
 //RUNTIME_OPTIONS --enable-preview -XX:+UseZGC -XX:NativeMemoryTracking=summary -XX:+HeapDumpOnOutOfMemoryError -XX:StartFlightRecording=filename=jfr/,path-to-gc-roots=true,jdk.ObjectCount#enabled=true
 
